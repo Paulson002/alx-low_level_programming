ca64550347b05d8426bd073418fc2a14e0534732
Paulson002 <dikeigwe00@gmail.com>
2022-12-15 03:33:48 -0800

    gorrit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,30 @@
-#include "main.h" 
-  
- /** 
-  * print_alphabet_x10 - print alphabet 
-  * 
-  * Return: Always 0. 
-  */ 
- void print_alphabet_x10(void) 
- { 
- int alphabet; 
- int count; 
-  
-  
- count = 0; 
- while (count < 10) 
- { 
- for (alphabet = 'a' ; alphabet <= 'z'; alphabet++) 
- { 
- _putchar(alphabet); 
- } 
-  
- count++; 
- _putchar('\n'); 
- } 
-  
-  
- }
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet in lower case
+ * followed by a new line
+ * Return: 0(sucess)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char letter = 'a';
+
+	int num = 1;
+
+	while (num < 11)
+	{
+		while (letter <= 'z')
+		{
+		putchar(letter);
+		letter++
+;		}
+	putchar('\n');
+	letter = 'a';
+	num++;
+;	}
+}
+
+
+
