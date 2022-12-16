831e72c404f3368bc00919fa0dc48ec59ffd74e0
Paulson002 <dikeigwe00@gmail.com>
2022-12-16 04:06:09 -0800

    forst

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,11 +4,11 @@
 /**
 *print_sign - print sign of number
 * @n: number to check
-*
+* main - prints the signs of numbers
 * Return: 1 if positive, 0 if zero, -1 if negative
 */
-
 int print_sign(int n)
+int main(void)
 {
 if (n > 0)
 {
@@ -26,5 +26,6 @@ else
 {
 putchar('-');
 return (-1);
+}
 }
 }
