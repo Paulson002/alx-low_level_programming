e513d022c06f44fa83356c6be2325070b928dfee
Paulson002 <dikeigwe00@gmail.com>
2022-12-16 07:40:19 -0800

    Hopefullyn i am

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -28,39 +28,23 @@ printf(" Fizz");
 
 }
 else if (i % 5 == 0 && i % 3 != 0)
-
 {
-
 printf(" Buzz");
-
 }
 else if (i % 3 == 0 && i % 5 == 0)
-
 {
-
 printf(" FizzBuzz");
-
 }
 else if (i == 1)
-
 {
-
 printf("%d", i);
-
 }
 else
-
 {
-
 printf(" %d", i);
-
 }
-
 }
-
 printf("\n");
 
-
 return (0);
-
 }
