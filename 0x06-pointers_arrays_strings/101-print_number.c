422f4e94b2f2fd8bf51138b111f236ceebc95926
Paulson002 <dikeigwe00@gmail.com>
2022-12-22 10:31:34 -0800

    gouy

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,6 +6,7 @@
  * Return: 0
  */
 
+
 void print_number(int n)
 
 {
@@ -19,18 +20,16 @@ if (n < 0)
 {
 
 _putchar('-');
-
 n1 = -n;
 
+}
 if (n1 / 10 != 0)
-
 {
 
 print_number(n1 / 10);
 
 }
-
 _putchar((n1 % 10) + '0');
+}
 
-}
-}
+
